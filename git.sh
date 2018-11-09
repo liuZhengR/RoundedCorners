ae89f95a923d0663e60d526452fdb41c1a325321
liuzhengrong <liuzhengrong@xiaoyezi.com>
2018-11-09 14:38:13 +0800

    提交

---
+++ git.sh
@@ -0,0 +1,7 @@
+echo "# RoundedCorners" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git remote add origin git@github.com:liuZhengR/RoundedCorners.git
+git push -u origin master
+
